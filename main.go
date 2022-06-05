22eddaba1c7219c249d1247d60c279add07b28b0
vishnu <vishnu@accuknox.com>
2022-06-05 20:05:50 +0530

    Updated code with checks for all parameters

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strings"
+	"time"
 
 	"github.com/common-nighthawk/go-figure"
 	"github.com/fatih/color"
@@ -11,26 +13,25 @@ import (
 	"github.com/urfave/cli/v2"
 	"github.com/vishnusomank/policy-cli-2.0/pkg/discover_op"
 	"github.com/vishnusomank/policy-cli-2.0/pkg/git_op"
+	"github.com/vishnusomank/policy-cli-2.0/resources"
 )
 
 func banner() {
-
-	figure.NewFigure("Knox AutoPol", "standard", true).Print()
 	fmt.Println()
 	fmt.Println()
-	fmt.Printf("[%s] Uses KubeConfig file to connect to cluster.\n", color.CyanString("WRN"))
-	fmt.Printf("[%s] Creates files and folders in current directory.\n", color.CyanString("WRN"))
+	fmt.Printf(strings.TrimSuffix(figure.NewFigure("Auto Discovery", "slant", true).String(), "\n") + "   v2.0.0")
+	fmt.Println()
+	fmt.Println()
 
 }
 
-var current_dir, git_dir, user_home, keyword, tags, ad_dir, git_username, git_token, git_repo_url, git_branch_name, git_repo_path, git_policy_name, git_base_branch string
-var policy_count int = 0
-var label_count int = 0
-var autoapply bool
-var version string = "1.0.0"
-var policy_updated *os.File
+func removeResidues(repo_path string) {
 
-const repo_path = "/tmp/accuknox-client-repo"
+	err := os.RemoveAll(repo_path)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 
@@ -48,18 +49,18 @@ func main() {
 	log.SetOutput(log_file)
 
 	// to get the current working directory
-	current_dir, err = os.Getwd()
+	resources.CURRENT_DIR, err = os.Getwd()
 	if err != nil {
 		log.Error(err)
 	}
 
 	// adding policy-template directory to current working directory
-	git_dir = current_dir + "/policy-template"
+	resources.GIT_DIR = resources.CURRENT_DIR + "/policy-template"
 
-	ad_dir = current_dir + "/ad-policy"
+	resources.AD_DIR = resources.CURRENT_DIR + "/ad-policy"
 
-	log.Info("Current Working directory: " + current_dir)
-	log.Info("Github clone directory: " + git_dir)
+	log.Info("Current Working directory: " + resources.CURRENT_DIR)
+	log.Info("Github clone directory: " + resources.GIT_DIR)
 
 	myFlags := []cli.Flag{
 		&cli.StringFlag{
@@ -147,24 +148,34 @@ func main() {
 		},
 	}
 	app := &cli.App{
-		Name:      "knox-autopol",
+		Name:      "Auto Discovery v2.0",
 		Usage:     "A simple CLI tool to automatically generate and apply policies or push to GitHub",
-		Version:   version,
-		UsageText: "knox-autopol [Flags]\nEg. knox-autopol --git_base_branch=deploy-branch --auto-apply=false --git_branch_name=temp-branch --git_token=gh_token123 --git_repo_url= https://github.com/testuser/demo.git --git_username=testuser",
+		Version:   resources.CLI_VERSION,
+		UsageText: "autodiscovery2.0 [Flags]\nEg. autodiscovery2.0 --git_base_branch=deploy-branch --auto-apply=false --git_branch_name=temp-branch --git_token=gh_token123 --git_repo_url= https://github.com/testuser/demo.git --git_username=testuser",
 		Flags:     myFlags,
 		Action: func(c *cli.Context) error {
-			git_username = c.String("git_username")
-			git_token = c.String("git_token")
-			git_repo_url = c.String("git_repo_url")
-			git_branch_name = c.String("git_branch_name")
-			autoapply = c.Bool("auto-apply")
-			git_base_branch = c.String("git_base_branch")
-			banner()
-			fileUrl := "https://raw.githubusercontent.com/accuknox/tools/main/install.sh"
-			discoverFileUrl := "https://raw.githubusercontent.com/accuknox/tools/main/get_discovered_yamls.sh"
-			git_op.Git_Operation(git_dir)
-			discover_op.Auto_Discover(fileUrl, discoverFileUrl, ad_dir, current_dir)
-			git_op.Init_Git(git_username, git_token, git_repo_url, git_branch_name, git_base_branch, repo_path, ad_dir, current_dir, autoapply)
+
+			if c.String("git_username") == "" || c.String("git_token") == "" || c.String("git_repo_url") == "" || c.String("git_branch_name") == "" || c.String("git_base_branch") == "" {
+				banner()
+				fmt.Printf("[%s][%s] Parameters missing.\n", color.BlueString(time.Now().Format("01-02-2006 15:04:05")), color.CyanString("WRN"))
+				fmt.Printf("[%s][%s] Please use autodisovery2.0 --help for help\n", color.BlueString(time.Now().Format("01-02-2006 15:04:05")), color.CyanString("WRN"))
+
+			} else {
+				banner()
+				fmt.Printf("[%s][%s] Uses KubeConfig file to connect to cluster.\n", color.BlueString(time.Now().Format("01-02-2006 15:04:05")), color.CyanString("WRN"))
+				fmt.Printf("[%s][%s] Creates files and folders in current directory.\n", color.BlueString(time.Now().Format("01-02-2006 15:04:05")), color.CyanString("WRN"))
+				fileUrl := "https://raw.githubusercontent.com/accuknox/samples/main/discover/install.sh"
+				discoverFileUrl := "https://raw.githubusercontent.com/accuknox/samples/main/discover/get_discovered_yamls.sh"
+				git_op.Git_Operation(resources.GIT_DIR)
+				discover_op.Auto_Discover(fileUrl, discoverFileUrl, resources.AD_DIR, resources.CURRENT_DIR)
+				resources.REPO_PATH = resources.CURRENT_DIR + resources.REPO_PATH
+				log.Info("repo_path=" + resources.REPO_PATH)
+				git_op.Init_Git(c.String("git_username"), c.String("git_token"), c.String("git_repo_url"), c.String("git_branch_name"), c.String("git_base_branch"), resources.REPO_PATH, resources.AD_DIR, resources.CURRENT_DIR, c.Bool("auto-apply"))
+				removeResidues(resources.GIT_DIR)
+				removeResidues(resources.AD_DIR)
+
+				removeResidues("logs.log")
+			}
 			return nil
 		},
 	}
